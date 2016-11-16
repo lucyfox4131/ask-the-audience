7effa352387267ca20b88750354983dadb523ebf
Lucy Fox <lucyreneefox@gmail.com>
2016-11-16 14:58:49 -0700

    Sending messages to every connected client

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const io = socketIo(server);
 
 io.on('connection', function(socket){
   console.log('A user has connected.', io.engine.clientsCount);
+  io.sockets.emit('usersConnected', io.engine.clientsCount);
   socket.on('disconnect', function(){
     console.log('A user has disconnected.', io.engine.clientsCount);
   });
